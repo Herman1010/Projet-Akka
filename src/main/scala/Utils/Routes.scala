8d86d4d7db185e3cb00c7a58e4691555697bd600
jeffdebruh <105702766+jeffdebruh@users.noreply.github.com>
2025-03-19 13:59:07 +0100

    feat update routes

--- src/main/scala/Utils/Routes.scala
+++ src/main/scala/Utils/Routes.scala
@@ -4,109 +4,12 @@ import ch.megard.akka.http.cors.scaladsl.CorsDirectives._
 import spray.json._
 import akka.http.scaladsl.marshalling.ToResponseMarshallable
 
-
 object Routes extends JsonFormats {
-  /* val route: Route =
-     path("api" / "message") {
-       get {
-         complete(Message("Hello, Akka with JSON!"))
-       }
-     } ~
-     path("api"/"message2"){
-             get {
-                 //complete(Message(UserDAO.getAll()))
-                 onSuccess(UserDAO.getAll()){
-                     utilisateur => complete(utilisateur.toJson)
-                 }
-             }
-         }
-  val route: Route =
-    path("api" / "message") {
-      get {
-        complete(Message("Hello, Akka with JSON!"))
-      }
-    } ~
-    path("api"/"message2"){
-            get {
-                //complete(Message(UserDAO.getAll()))
-                onSuccess(UserDAO.getAll()){
-                    utilisateur => complete(utilisateur.toJson)
-                }
-            }
-        } ~
-      path("api"/ "notif"){
-        get {
-          onSuccess(NotifDAO.getAll()){
-            notifications => complete(notifications.toJson)
-          }
-        }
-      } ~
-      path("api" / "ActiveCourses"){
-        get {
-          onSuccess(ActiveCoursesDAO.getAll()){
-            activeCourses => complete(activeCourses.toJson)
-          }
-        }
-      }~
-      path("api"/"portefeuilles"){
-            get {
-                onSuccess(PortefeuilleDAO.getAll()){
-                    portefeuille => complete(portefeuille.toJson)
-                }
-            }
-        }~
-   path("api" / "portefeuilles" / Segment) { userId =>
-  get {
-    onSuccess(PortefeuilleDAO.getByUtilisateurrId(userId)) { portefeuille =>
-      complete(portefeuille.toJson)
-    }
-  }
-}
-
-        }~
-      path("api"/"actifs"){
-            get {
-                onSuccess(ActifDAO.getAll()){
-                    actif => complete(actif.toJson)
-                }
-            }
-        }
-         } ~
-       path("api"/ "notif"){
-         get {
-           onSuccess(NotifDAO.getAll()){
-             notifications => complete(notifications.toJson)
-           }
-         }
-       } ~
-       path("api" / "ActiveCourses"){
-         get {
-           onSuccess(ActiveCoursesDAO.getAll()){
-             activeCourses => complete(activeCourses.toJson)
-           }
-         }
-       }
-       path("api"/"portefeuilles"){
-             get {
-                 //complete(Message(UserDAO.getAll()))
-                 onSuccess(PortefeuilleDAO.getAll()){
-                     portefeuille => complete(portefeuille.toJson)
-                 }
-             }
-         }
-       path("api"/"actifs"){
-             get {
-                 //complete(Message(UserDAO.getAll()))
-                 onSuccess(ActifDAO.getAll()){
-                     actif => complete(actif.toJson)
-                 }
-             }
-         }
- */
+  //val route: Route =
   val routes = cors() {
     pathPrefix("api" / "message2") {
       get {
-        onSuccess(UserDAO.getAll()){
+        onSuccess(UserDAO.getAll()) {
           utilisateur => complete(utilisateur.toJson)
         } ~
           delete {
@@ -115,7 +18,42 @@ object Routes extends JsonFormats {
             complete(Message("Hello, Akka with JSON!"))
           }
       }
-    }
+    } ~
+      pathPrefix("api" / "message") {
+        get {
+          complete(Message("Hello, Akka with JSON!"))
+        }
+      } ~
+      path("api" / "notif") {
+        get {
+          onSuccess(NotifDAO.getAll()) {
+            notifications => complete(notifications.toJson)
+          }
+        }
+      } ~
+      pathPrefix("api" / "ActiveCourses") {
+        get {
+          onSuccess(ActiveCoursesDAO.getAll()) {
+            activeCourses => complete(activeCourses.toJson)
+          }
+        }
+      }~
+      pathPrefix("api" / "portefeuilles") {
+        get {
+          //complete(Message(UserDAO.getAll()))
+          onSuccess(PortefeuilleDAO.getAll()) {
+            portefeuille => complete(portefeuille.toJson)
+          }
+        }
+      }~
+      pathPrefix("api" / "actifs") {
+        get {
+          //complete(Message(UserDAO.getAll()))
+          onSuccess(ActifDAO.getAll()) {
+            actif => complete(actif.toJson)
+          }
+        }
+      }
   }
 }
 
