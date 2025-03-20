5cb94f66074786f46893cd0160dfeab8491ff6ea
jeffdebruh <105702766+jeffdebruh@users.noreply.github.com>
2025-03-20 22:39:23 +0100

    Update Navbar.js

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -8,7 +8,7 @@ const Navbar = () => {
                 <li style={styles.navItem}><a style={styles.navLink} href="/login">Login</a></li>
                 <li style={styles.navItem}><a style={styles.navLink} href="/portfolio">Portfolio</a></li>
                 <li style={styles.navItem}><a style={styles.navLink} href="/articles">Article</a></li>
-                <li style={styles.navItem}><a style={styles.navLink} href="/clients">Client</a></li>
+                <li style={styles.navItem}><a style={styles.navLink} href="/portefeuille">Portefeuille</a></li>
             </ul>
         </nav>
     );
